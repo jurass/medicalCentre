775ee444d1be02c6f07705234d5f63050585fc72
Monika Jurkiewicz <m4.jurkiewicz@gmail.com>
2015-05-18 14:16:28 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MedicalCentre::Application.config.secret_token = 'abfe68b8f9bb451082a5f7c44d48d61961b3a1c6a32f8dcaea009ff4cde7c84a444844b43ede0a04724f01a6867317c5e2eba5a7471dc2a50d29b6229bfe2f95'
